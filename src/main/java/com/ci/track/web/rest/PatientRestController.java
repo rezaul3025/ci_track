39f70cd8eb572ceea1b0d1db2e77026c3dc2ccef
rkarim <rezaul.karim@gmail.com>
2017-11-30 04:32:52 +0100

    init

--- src/main/java/com/ci/track/web/rest/PatientRestController.java
+++ src/main/java/com/ci/track/web/rest/PatientRestController.java
@@ -2,7 +2,9 @@ package com.ci.track.web.rest;
 
 import java.util.Date;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ci.track.web.event.PatientInfo;
@@ -14,5 +16,11 @@ public class PatientRestController {
 	@RequestMapping("/info")
 	public PatientInfo getPatientInfo(){
 		return new PatientInfo("Tom","Mia",new Date(), "Tom put");
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public PatientInfo addInfo(@RequestBody PatientInfo patientInfo){
+		
+		return patientInfo;
 	}
 }
