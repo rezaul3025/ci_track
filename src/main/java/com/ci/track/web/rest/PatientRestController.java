4364761da392ac67e4d56e461573823239c243c2
rkarim <rezaul.karim@gmail.com>
2017-12-22 13:13:52 +0100

    init

--- src/main/java/com/ci/track/web/rest/PatientRestController.java
+++ src/main/java/com/ci/track/web/rest/PatientRestController.java
@@ -1,10 +1,20 @@
 package com.ci.track.web.rest;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,11 +41,38 @@ public class PatientRestController {
 	@RequestMapping(value="/add", method=RequestMethod.POST)
 	public PatientInfo addInfo(@RequestBody PatientInfo patientInfo){
 		
-		String sql = "INSERT INTO patient " +
-				" VALUES (?, ?, ?,?,?)";
-		jdbcTemplate.update(sql, new Object[] { 2,
-				patientInfo.getFirstName(),patientInfo.getLastName(), patientInfo.getDob(), patientInfo.getAddress()});
+		LocalDate today = LocalDate.now();
+		LocalDate birthday = patientInfo.getDob().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		 
+		Period p = Period.between(birthday, today);
+		 
+		//Now access the values as below
+		System.out.println(p.getDays());
+		System.out.println(p.getMonths());
+		System.out.println(p.getYears());
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		/*String sql = "INSERT INTO patient(firstName, lastName, dob, address) " +
+				" VALUES (?,?,?,?)";
+		jdbcTemplate.update(sql, new Object[] {
+				patientInfo.getFirstName(),patientInfo.getLastName(), patientInfo.getDob(), patientInfo.getAddress()}, keyHolder);
+		*/
 		
+		final PreparedStatementCreator psc = new PreparedStatementCreator() {
+		      @Override
+		      public PreparedStatement createPreparedStatement(final Connection connection) throws SQLException {
+		        final PreparedStatement ps = connection.prepareStatement("INSERT INTO patient(firstName, lastName, dob, address) " +
+						" VALUES (?,?,?,?)",
+		            Statement.RETURN_GENERATED_KEYS);
+		        ps.setString(1, patientInfo.getFirstName());
+		        ps.setString(2, patientInfo.getLastName());
+		        ps.setObject(3, patientInfo.getDob());
+		        ps.setString(4, patientInfo.getAddress());
+		        return ps;
+		      }
+		    };
+		
+		jdbcTemplate.update(psc, keyHolder);
+		    
 		System.out.println(patientInfo);
 		
 		return patientInfo;
