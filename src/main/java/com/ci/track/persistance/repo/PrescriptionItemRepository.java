96e8b0928974022e5faabeabdfa193ad750f8823
rkarim <rezaul.karim@gmail.com>
2018-01-20 05:54:27 +0100

    init

---
+++ src/main/java/com/ci/track/persistance/repo/PrescriptionItemRepository.java
@@ -0,0 +1,12 @@
+package com.ci.track.persistance.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ci.track.persistance.domain.PrescriptionItem;
+
+
+@Repository
+public interface PrescriptionItemRepository extends JpaRepository<PrescriptionItem, Integer>{
+
+}
--- src/main/java/com/ci/track/persistance/service/PrescriptionServiceHandler.java
+++ src/main/java/com/ci/track/persistance/service/PrescriptionServiceHandler.java
@@ -1,12 +1,17 @@
 package com.ci.track.persistance.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ci.track.persistance.domain.Prescription;
+import com.ci.track.persistance.domain.PrescriptionItem;
 import com.ci.track.persistance.repo.PrescriptionRepository;
 import com.ci.track.web.event.PrescriptionInfo;
+import com.ci.track.web.event.PrescriptionItemInfo;
 
 @Service
 public class PrescriptionServiceHandler implements PrescriptionService {
@@ -18,6 +23,15 @@ public class PrescriptionServiceHandler implements PrescriptionService {
 	public Prescription addPrescription(PrescriptionInfo prescriptionInfo ){
 		Prescription prescription = new Prescription();
 		BeanUtils.copyProperties(prescriptionInfo, prescription);
+		
+		List<PrescriptionItem> prescriptionItemList = new ArrayList<>();
+		for(PrescriptionItemInfo prescriptionItemInfo : prescriptionInfo.getPrescriptionItem()){
+			PrescriptionItem pescriptionItem = new PrescriptionItem();
+			BeanUtils.copyProperties(prescriptionItemInfo, pescriptionItem);
+			prescriptionItemList.add(pescriptionItem);
+		}
+		
+		prescription.setPrescriptionItem(prescriptionItemList);
 
 		prescriptionRepository.saveAndFlush(prescription);
 		
